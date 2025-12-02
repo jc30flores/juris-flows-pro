594d146d843f8a12975a6de997f405e6712000fd
Juan Flores <74993575+jc30flores@users.noreply.github.com>
2025-12-02 07:43:19 -0600

    Force backend runserver port and add trusted origins

--- backend/manage.py
+++ backend/manage.py
@@ -12,6 +12,8 @@ def main():
             "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) == 2 and sys.argv[1] == "runserver":
+        sys.argv.append("127.0.0.1:8007")
     execute_from_command_line(sys.argv)
 
 
